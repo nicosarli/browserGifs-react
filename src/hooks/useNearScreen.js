97cba676a7b6f9425b439c6d0b928c1d805f5e3a
nicosarli <nico_sarli@hotmail.com>
2021-08-04 10:52:17 -0300

    Agregando la dependencia debounce para limitar la cantidad de llamdadas de una funcion, utilizando useCallBack y creando un infinite scroll en las busquedas

--- src/hooks/useNearScreen.js
+++ src/hooks/useNearScreen.js
@@ -1,15 +1,23 @@
 import React, { useEffect, useState, useRef } from "react";
 
-export default function useNearScreen({ distance = "100px" } = {}) {
+export default function useNearScreen({
+  distance = "100px",
+  externalRef,
+  once = true,
+} = {}) {
   const [isNearScreen, setShow] = useState(false);
   const fromRef = useRef();
 
   useEffect(() => {
+    const element = externalRef ? externalRef.current : fromRef.current;
+
     const onChange = (entries, observer) => {
       const el = entries[0];
       if (el.isIntersecting) {
         setShow(true);
-        observer.disconnect();
+        once && observer.disconnect();
+      } else {
+        !once && setShow(false);
       }
     };
 
@@ -17,7 +25,7 @@ export default function useNearScreen({ distance = "100px" } = {}) {
       rootMargin: distance,
     });
 
-    observer.observe(fromRef.current);
+    element && observer.observe(element);
 
     return () => observer.disconnect();
   });
--- src/pages/SearchResults/index.js
+++ src/pages/SearchResults/index.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 import ListOfGifs from "../../components/ListOfGifs/ListOfGifs";
 import useGifs from "../../hooks/useGifs";
+import useNearScreen from "../../hooks/useNearScreen";
+import debounce from "just-debounce-it";
 
 export default function SearchResults({ params }) {
   const { keyword } = params;
   const { loading, gifs, setPage } = useGifs({ keyword });
+  const externalRef = useRef();
+  const { isNearScreen } = useNearScreen({
+    externalRef: loading ? null : externalRef,
+    once: false,
+  });
 
-  const handleNextPage = () => {
-    setPage((prevPage) => prevPage + 1);
-  };
+  const debouceHandleNextPage = useCallback(
+    debounce(() => setPage((prevPage) => prevPage + 1), 1000),
+    []
+  );
+
+  useEffect(() => {
+    if (isNearScreen) debouceHandleNextPage();
+    console.log(isNearScreen);
+  }, [debouceHandleNextPage, isNearScreen]);
 
   return (
     <>
-      {loading ? <h1>Cargando...</h1> : <ListOfGifs gifs={gifs} />}
-      <button onClick={handleNextPage}>Get next page</button>
+      {loading ? (
+        <h1>Cargando...</h1>
+      ) : (
+        <>
+          <ListOfGifs gifs={gifs} />
+          <div id="visor" ref={externalRef}></div>
+        </>
+      )}
     </>
   );
 }
