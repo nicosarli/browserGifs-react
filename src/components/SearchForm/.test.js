6af81912c4602de7f61136dd341508da2b299e52
nicosarli <nico_sarli@hotmail.com>
2021-08-06 12:22:45 -0300

    Agregando dos test mas

--- src/components/SearchForm/.test.js
+++ src/components/SearchForm/.test.js
@@ -1,12 +1,36 @@
 import {renderHook, act} from '@testing-library/react-hooks'
 import useForm from './hook'
 
+const setup = (params) => renderHook(() => useForm(params))
+
 test('should change keyword', () => {
-  const { result }  = renderHook(() => useForm())
+  const { result }  = setup()
 
   act(() => {
     result.current.updateKeyword('batman')
   })
 
   expect(result.current.keyword).toBe('batman')
+})
+
+test('should use initial values', () => {
+  const { result }  = setup({
+    initialKeyword: 'matrix'
+  })
+
+  expect(result.current.keyword).toBe('matrix')
+})
+
+test('should update correctly times when used twice', () => {
+  const { result }  = setup({
+    initialKeyword: 'matrix'
+  })
+
+  act(() => {
+    result.current.updateKeyword('b')
+    result.current.updateKeyword('ba')
+  })
+
+  expect(result.current.keyword).toBe('ba')
+  expect(result.current.times).toBe(2)
 })
